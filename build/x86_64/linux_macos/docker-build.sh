80beac2134b523b4ecd481a2ad10dd6c354e38f2
neomatrix369 <sadhak001@gmail.com>
2018-06-11 18:46:21 +0100

    Passing RUN_TESTS param to the docker container, also displaying all the parameters passed in and used by the docker-build.sh script

--- build/x86_64/linux_macos/docker-build.sh
+++ build/x86_64/linux_macos/docker-build.sh
@@ -21,7 +21,7 @@ mkdir -p "${HOST_OUTPUT_DIR}"
 BUILD_LOGS="${HOST_OUTPUT_DIR}/docker-build.logs"
 
 CONTAINER_SCRIPTS_DIR="${CONTAINER_HOME_DIR}/scripts"
-CONTAINER_OUTPUT_DIR="${CONTAINER_HOME_DIR}/output"
+CONTAINER_OUTPUT_DIR=${OUTPUT_DIR:-"${CONTAINER_HOME_DIR}/output"}
 
 echo "*************************************************"
 echo "* "
@@ -32,12 +32,29 @@ echo "* tail -f ${HOST_OUTPUT_DIR}/docker-build.logs"
 echo "* "
 echo "*************************************************"
 
+echo "******************* Parameters ******************"
+echo "DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG}"
+echo "JDK_VERSION=${JDK_VERSION}"
+echo "DEBUG=${DEBUG}"
+echo "DEBUG_MODE_ARGS=${DEBUG_MODE_ARGS}"
+echo ""
+echo "HOST_OUTPUT_DIR=${HOST_OUTPUT_DIR}"
+echo "CONTAINER_HOME_DIR=${CONTAINER_HOME_DIR}"
+echo "CONTAINER_SCRIPTS_DIR=${CONTAINER_SCRIPTS_DIR}"
+echo "CONTAINER_OUTPUT_DIR=${CONTAINER_OUTPUT_DIR}"
+echo ""
+echo "BUILD_LOGS=${BUILD_LOGS}"
+echo "OUTPUT_DIR=${OUTPUT_DIR:-}"
+echo "RUN_TESTS=${RUN_TESTS:-}"
+echo "*************************************************"
+
 docker build -t ${DOCKER_IMAGE_TAG} .
 docker run                               \
        ${DEBUG_MODE_ARGS}                \
        --rm                              \
        --env JAVA_VERSION=${JDK_VERSION} \
        --env OUTPUT_DIR=${CONTAINER_OUTPUT_DIR}             \
+       --env RUN_TESTS=${RUN_TESTS:-""}                     \
        --volume $(pwd):${CONTAINER_SCRIPTS_DIR}             \
        --volume ${HOST_OUTPUT_DIR}:${CONTAINER_OUTPUT_DIR}  \
        ${DOCKER_IMAGE_TAG} &> ${BUILD_LOGS}
