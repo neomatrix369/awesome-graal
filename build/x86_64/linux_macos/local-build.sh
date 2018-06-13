47d7cd2357f11b04ea851645227fd155be5224ed
neomatrix369 <sadhak001@gmail.com>
2018-06-14 00:00:11 +0100

    Adapt the sed command to apply the patches for graaljs and graalpython when the OS is linux

--- build/x86_64/linux_macos/local-build.sh
+++ build/x86_64/linux_macos/local-build.sh
@@ -97,10 +97,18 @@ buildGraalVMSuite() {
     echo ">>> Building GraalVM Suite"
     cd ${BASEDIR}/graal/vm
     echo "Applying temporary patch to fix graaljs cloning issue in 'mx.vm/suite.py'"
-    sed -i '' 's/https:\/\/github.com\/oracle\/js.git/https:\/\/github.com\/graalvm\/graaljs.git/g' mx.vm/suite.py
+    if [[ "$(uname)" = "Darwin" ]]; then
+       sed -i '' 's/https:\/\/github.com\/oracle\/js.git/https:\/\/github.com\/graalvm\/graaljs.git/g' mx.vm/suite.py
+    else
+       sed -i 's https://github.com/oracle/js.git https://github.com/graalvm/graaljs.git ' mx.vm/suite.py
+    fi
 
     echo "Applying temporary patch to fix graalpython cloning issue in 'mx.vm/suite.py'"
-    sed -i '' 's/https:\/\/github.com\/oracle\/graalpython.git/https:\/\/github.com\/graalvm\/graalpython.git/g' mx.vm/suite.py
+    if [[ "$(uname)" = "Darwin" ]]; then
+        sed -i '' 's/https:\/\/github.com\/oracle\/graalpython.git/https:\/\/github.com\/graalvm\/graalpython.git/g' mx.vm/suite.py
+    else
+        sed -i 's https://github.com/oracle/graalpython.git https://github.com/graalvm/graalpython.git ' mx.vm/suite.py
+    fi
 
     ${MX} --dy /substratevm,/tools,sulong,/graal-nodejs,/fastr,truffleruby,graalpython build
 }
