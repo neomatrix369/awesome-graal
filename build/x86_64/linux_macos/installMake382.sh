cf3ad02819d02ab296aee26cc48bac21da80b0ed
neomatrix369 <sadhak001@gmail.com>
2018-06-17 19:06:34 +0100

    Added additional echos and checks to installMake382.sh

--- build/x86_64/linux_macos/installMake382.sh
+++ build/x86_64/linux_macos/installMake382.sh
@@ -1,6 +1,33 @@
-wget https://ftp.gnu.org/gnu/make/make-3.82.tar.gz
-tar xvzf make-3.82.tar.gz
-cd make-3.82
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+MAKE_ARTIFACT_NAME=make-3.82
+MAKE_ARTIFACT="${MAKE_ARTIFACT_NAME}.tar.gz"
+
+if [[ ! -f "${MAKE_ARTIFACT}" ]]; then
+	echo "Downloading ${MAKE_ARTIFACT}, since it's not found"
+	wget https://ftp.gnu.org/gnu/make/${MAKE_ARTIFACT}
+else
+	echo "${MAKE_ARTIFACT} already exists"
+fi
+
+if [[ ! -d "${MAKE_ARTIFACT_NAME}" ]]; then
+    echo "Unpacking ${MAKE_ARTIFACT_NAME}"
+	tar xvzf ${MAKE_ARTIFACT}
+else
+    echo "${MAKE_ARTIFACT_NAME} already unpacked"
+fi
+
+cd ${MAKE_ARTIFACT_NAME}
+
+echo "Running configure for make"
 ./configure
+
+echo "Building make"
 make
+
+echo "Installing make"
 make install
