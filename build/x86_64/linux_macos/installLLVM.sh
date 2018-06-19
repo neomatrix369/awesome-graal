6fb65b5ce02529aa2e5ec840304aeb12337abb2a
neomatrix369 <sadhak001@gmail.com>
2018-06-19 23:32:11 +0100

    Switch to LLVM version 6.0, detect docker env and not use sudo, minor refactoring of the apt-get install packages list

--- build/x86_64/linux_macos/installLLVM.sh
+++ build/x86_64/linux_macos/installLLVM.sh
@@ -4,19 +4,29 @@ set -e
 set -u
 set -o pipefail
 
-LLVM_VERSION=${1:-6.0}
+### Note only version 5.0 and 6.0 are available at the time of creation of this script, see https://apt.llvm.org/
+LLVM_VERSION=6.0
+
+if [ -f "/.dockerenv" ]; then
+   echo "Running inside the container environment i.e. Docker"
+   sudoCMD=""
+else
+   echo "Running in a real OS environment"
+   sudoCMD="sudo"
+fi
 
 echo "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-${LLVM_VERSION} main" >> /etc/apt/sources.list.d/llvm.org.list
 echo "deb-src http://apt.llvm.org/xenial/ llvm-toolchain-xenial-${LLVM_VERSION} main" >> /etc/apt/sources.list.d/llvm.org.list
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | ${sudoCMD} apt-key add -
 
-sudo apt-get update && \
-    apt-get install -y \
+${sudoCMD} apt-get update && \
+    ${sudoCMD} apt-get install -y \
     clang-${LLVM_VERSION} clang-tools-${LLVM_VERSION} clang-${LLVM_VERSION}-doc \
+    clang-format-${LLVM_VERSION} python-clang-${LLVM_VERSION} \
     libclang-common-${LLVM_VERSION}-dev libclang-${LLVM_VERSION}-dev libclang1-${LLVM_VERSION} \ 
-    libllvm6.0 lldb-${LLVM_VERSION} llvm-${LLVM_VERSION} llvm-${LLVM_VERSION}-dev \
+    libllvm${LLVM_VERSION} lldb-${LLVM_VERSION} llvm-${LLVM_VERSION} llvm-${LLVM_VERSION}-dev \
     llvm-${LLVM_VERSION}-doc llvm-${LLVM_VERSION}-examples llvm-${LLVM_VERSION}-runtime \
-    clang-format-${LLVM_VERSION} python-clang-${LLVM_VERSION} lld-${LLVM_VERSION} libfuzzer-${LLVM_VERSION}-dev libc++-dev
+    lld-${LLVM_VERSION} libfuzzer-${LLVM_VERSION}-dev libc++-dev
 
 echo ">>> Warning: Could not find these packages libllvm-${LLVM_VERSION}-ocaml-dev lldb-${LLVM_VERSION}-dev, even though mentioned on https://apt.llvm.org/ <<<"
