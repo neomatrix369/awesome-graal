4ef16ca888563d36038ebc0c1eabc71779820f16
neomatrix369 <sadhak001@gmail.com>
2018-06-23 21:16:58 +0100

    Revert changes to git clone, for now lets shallow clone

--- build/x86_64/linux_macos/lib/utils.sh
+++ build/x86_64/linux_macos/lib/utils.sh
@@ -16,7 +16,6 @@ gitClone() {
     org=$1
     repo=$2
     programDesc=$3
-    tag=${4:-master}
 
     if [[ -e "${repo}/.git" ]]; then
         echo ">>> ${repo} already exists: updating and using this version"
@@ -25,7 +24,6 @@ gitClone() {
         git pull
     else
         echo ">>> Getting ${repo}: ${programDesc}"
-        git clone https://github.com/${org}/${repo}.git
+        git clone --depth=1 https://github.com/${org}/${repo}.git
     fi
-    git checkout ${tag}
 }
