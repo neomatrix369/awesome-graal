5053825db7d411b119089321e724d5406b3e7710
neomatrix369 <sadhak001@gmail.com>
2018-06-23 12:57:28 +0100

    Improved displayDependencyVersion.sh to display hardware and software details of the machine its running on

--- build/x86_64/linux_macos/lib/displayDependencyVersion.sh
+++ build/x86_64/linux_macos/lib/displayDependencyVersion.sh
@@ -6,6 +6,43 @@ set -o pipefail
 
 source ${SCRIPTS_LIB_DIR}/utils.sh
 
+printHWInfo() {
+    echo ""
+    echo "Display hardware information"
+    lscpu
+    echo ""
+    free -m
+    free -m -h
+}
+
+printRuntimeEnvInfo() {
+    dockerContainer=""
+    if [[ -f "/.dockerenv" ]]; then
+        dockerContainer="inside a docker container "
+    fi
+
+    runningInsideA_VM="$(cat /proc/cpuinfo | grep hypervisor || true)"
+    if [[ -z "${runningInsideA_VM}" ]]; then
+        machine="bare-metal (native)"
+    else
+        machine="VM or VM-like"
+    fi
+
+    echo ""
+    echo ">>> Processes are running ${dockerContainer}on a ${machine} environment  <<<"
+}
+
+printOSInfo() {
+    echo ""
+    echo "Display OS information"
+    uname -a
+
+    printRuntimeEnvInfo
+}
+
+printHWInfo
+printOSInfo
+
 versionCheck java
 versionCheck make
 versionCheck python "--version"
