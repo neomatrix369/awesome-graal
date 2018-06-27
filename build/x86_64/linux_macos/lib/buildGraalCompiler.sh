8d6599126f15294b8d1d34b991751b2ec205080d
neomatrix369 <sadhak001@gmail.com>
2018-06-28 00:57:57 +0100

    Pass the ${JAVA_HOME} as param to buildGraalCompiler.sh and the mx build command

--- build/x86_64/linux_macos/lib/buildGraalCompiler.sh
+++ build/x86_64/linux_macos/lib/buildGraalCompiler.sh
@@ -9,6 +9,7 @@ source ${SCRIPTS_LIB_DIR}/utils.sh
 BASEDIR=$1
 MX=$2
 BUILD_ARTIFACTS_DIR=$3
+JAVA_HOME=$4
 echo ">>> Building Graal"
 cd ${BASEDIR}
 gitClone oracle \
@@ -20,5 +21,5 @@ export JVMCI_VERSION_CHECK='ignore'
 echo ">>>> Setting environment variable JVMCI_VERSION_CHECK=${JVMCI_VERSION_CHECK}"
 HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS:-$(getAllowedThreads)}
 echo "Setting HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS}"
-HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS} ${MX} build
-${MX} makegraaljdk --force ${BUILD_ARTIFACTS_DIR}
+HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS} JAVA_HOME=${JAVA_HOME} ${MX} build --java-home ${JAVA_HOME}
+${MX} makegraaljdk --force ${BUILD_ARTIFACTS_DIR} 
