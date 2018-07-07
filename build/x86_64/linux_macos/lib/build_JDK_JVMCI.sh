db604b9213960d7b104644cb7eb72bede2b582c5
neomatrix369 <sadhak001@gmail.com>
2018-07-07 04:22:22 +0100

    Added newline notation, fixed failing script

--- build/x86_64/linux_macos/lib/build_JDK_JVMCI.sh
+++ build/x86_64/linux_macos/lib/build_JDK_JVMCI.sh
@@ -12,7 +12,7 @@ MX=$2
 cd ${BASEDIR}
 gitClone graalvm       \
          graal-jvmci-8 \
-         "Getting Graal JVMCI for JDK8"
+         "Getting Graal JVMCI for JDK8" \
          "doNotUpdate"
 
 echo ">>> Building a JDK8 with JVMCI..."
