287e8317962d454aa589bc4abe52faac2e62c506
neomatrix369 <sadhak001@gmail.com>
2018-07-14 00:13:52 +0100

    Amend the assignment value to GRAALVM_SUITE_RUNTIMES and to .circleci/config.yml

--- build/x86_64/linux_macos/local-build.sh
+++ build/x86_64/linux_macos/local-build.sh
@@ -15,7 +15,7 @@ export RUN_TESTS=${RUN_TESTS:-"true"}
 JDK_GRAAL_FOLDER_NAME=jdk8-with-graal
 export GRAAL_JVMCI_8_TAG=jvmci-0.46
 BUILD_ARTIFACTS_DIR=${BASEDIR}/${JDK_GRAAL_FOLDER_NAME}
-GRAALVM_SUITE_RUNTIMES=${GRAALVM_SUITE_RUNTIMES:-"/substratevm,/tools,sulong,/graal-nodejs,/fastr,truffleruby,graalpython"}
+GRAALVM_SUITE_RUNTIMES=${GRAALVM_SUITE_RUNTIMES:-'/substratevm,/tools,sulong,/graal-nodejs,/fastr,truffleruby,graalpython'}
 
 export JAVA_OPTS="$(echo ${DOCKER_JAVA_OPTS:-""} ${JAVA_OPTS})"
 export FASTR_RELEASE=true
