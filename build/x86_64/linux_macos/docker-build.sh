adfb360c6ed08735d0cce7868e7ed621a78b946d
neomatrix369 <sadhak001@gmail.com>
2018-07-31 00:47:40 +0100

    Extract docker run container command out from then docker-build.sh script into its own script

--- build/x86_64/linux_macos/docker-build.sh
+++ build/x86_64/linux_macos/docker-build.sh
@@ -4,32 +4,32 @@ set -e
 set -u
 set -o pipefail
 
-DEBUG=${DEBUG:-"false"}
-RUN_TESTS=${RUN_TESTS:-"true"}
+export DEBUG=${DEBUG:-"false"}
+export RUN_TESTS=${RUN_TESTS:-"true"}
 
 export JAVA_VERSION=${JAVA_VERSION:-jdk8u152-b16}
 export JDK_BASE_IMAGE_TAG=${JDK_BASE_IMAGE_TAG:-"openjdk8:${JAVA_VERSION}"}
 export DOCKER_IMAGE_TAG="graalvm-suite-jdk8:latest"
 export USER_IN_CONTAINER=${USER_IN_CONTAINER:-"graal"}
-CONTAINER_HOME_DIR="/home/${USER_IN_CONTAINER}"
+export CONTAINER_HOME_DIR="/home/${USER_IN_CONTAINER}"
 export MAKE_VERSION=${MAKE_VERSION:-3.82}
 export LLVM_VERSION=${LLVM_VERSION:-6.0}
 export RUBY_VERSION=${RUBY_VERSION:-2.2.2}
-GRAALVM_SUITE_RUNTIMES=${GRAALVM_SUITE_RUNTIMES:-"/substratevm,/tools,sulong,/graal-nodejs,/fastr,truffleruby,graalpython"}
-DOCKER_MEMORY=2048M
+export GRAALVM_SUITE_RUNTIMES=${GRAALVM_SUITE_RUNTIMES:-"/substratevm,/tools,sulong,/graal-nodejs,/fastr,truffleruby,graalpython"}
+export DOCKER_MEMORY=2048M
 export DOCKER_JAVA_OPTS="-Xms300m -Xmx300m -XX:+PrintFlagsFinal"
 
-HOST_REPOS_DIR=${HOST_REPOS_DIR:-""}
+export HOST_REPOS_DIR=${HOST_REPOS_DIR:-""}
 if [[ ! -z "${HOST_REPOS_DIR}" ]]; then
-    HOST_REPOS_DIR="$(pwd)/jdk8-with-graal-repos"
+    export HOST_REPOS_DIR="$(pwd)/jdk8-with-graal-repos"
 fi
 
-HOST_OUTPUT_DIR="$(pwd)/jdk8-with-graal-docker"
+export HOST_OUTPUT_DIR="$(pwd)/jdk8-with-graal-docker"
 mkdir -p "${HOST_OUTPUT_DIR}"
-BUILD_LOGS="${HOST_OUTPUT_DIR}/docker-build.logs"
+export BUILD_LOGS="${HOST_OUTPUT_DIR}/docker-build.logs"
 
-CONTAINER_SCRIPTS_DIR="${CONTAINER_HOME_DIR}/scripts"
-CONTAINER_OUTPUT_DIR="${CONTAINER_HOME_DIR}/output"
+export CONTAINER_SCRIPTS_DIR="${CONTAINER_HOME_DIR}/scripts"
+export CONTAINER_OUTPUT_DIR="${CONTAINER_HOME_DIR}/output"
 
 echo "*************************************************"
 echo "* "
@@ -68,46 +68,7 @@ echo "*************************************************"
 
 ./build-docker-image.sh
 
-HOST_REPOS_DIR_DOCKER_PARAM=""
-if [[ ! -z "${HOST_REPOS_DIR}" ]]; then
-    mkdir -p "${HOST_REPOS_DIR}"
-    HOST_REPOS_DIR_DOCKER_PARAM="--volume ${HOST_REPOS_DIR}:${CONTAINER_HOME_DIR}"
-fi
-
-if [[ "${DEBUG}" = "true" ]]; then
-  echo "* Running container ${DOCKER_IMAGE_TAG} in DEBUG mode"
-  docker run                                                       \
-         --rm                                                      \
-         --interactive --tty --entrypoint /bin/bash                \
-         --user ${USER_IN_CONTAINER}                               \
-         --env SCRIPTS_LIB_DIR=${CONTAINER_HOME_DIR}/scripts/lib   \
-         --env OUTPUT_DIR="${CONTAINER_OUTPUT_DIR}"                \
-         --env RUN_TESTS="${RUN_TESTS}"                            \
-         --env GRAALVM_SUITE_RUNTIMES="${GRAALVM_SUITE_RUNTIMES}"  \
-         --env JAVA_OPTS="${DOCKER_JAVA_OPTS}"              \
-         --volume $(pwd):${CONTAINER_SCRIPTS_DIR}                  \
-         --volume $(pwd)/patch:${CONTAINER_HOME_DIR}/patch         \
-         --volume ${HOST_OUTPUT_DIR}:${CONTAINER_OUTPUT_DIR}       \
-         --memory ${DOCKER_MEMORY}                                              \
-         ${HOST_REPOS_DIR_DOCKER_PARAM}                            \
-         ${DOCKER_IMAGE_TAG}
-else   
-  docker run                                                         \
-         --rm                                                        \
-         --user ${USER_IN_CONTAINER}                                 \
-         --entrypoint "${CONTAINER_HOME_DIR}/scripts/local-build.sh" \
-         --env SCRIPTS_LIB_DIR=${CONTAINER_HOME_DIR}/scripts/lib     \
-         --env OUTPUT_DIR="${CONTAINER_OUTPUT_DIR}"                  \
-         --env RUN_TESTS="${RUN_TESTS}"                              \
-         --env GRAALVM_SUITE_RUNTIMES="${GRAALVM_SUITE_RUNTIMES}"    \
-         --env JAVA_OPTS="${DOCKER_JAVA_OPTS}"                \
-         --volume $(pwd):${CONTAINER_SCRIPTS_DIR}                    \
-         --volume $(pwd)/patch:${CONTAINER_HOME_DIR}/patch           \
-         --volume ${HOST_OUTPUT_DIR}:${CONTAINER_OUTPUT_DIR}         \
-         --memory ${DOCKER_MEMORY}                                   \
-         ${HOST_REPOS_DIR_DOCKER_PARAM}                              \
-         ${DOCKER_IMAGE_TAG} &> ${BUILD_LOGS}
-fi
+./run-docker-container.sh
 
 echo "*************************************************"
 echo "* "
---
+++ build/x86_64/linux_macos/run-docker-container.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+HOST_REPOS_DIR_DOCKER_PARAM=""
+if [[ ! -z "${HOST_REPOS_DIR}" ]]; then
+    mkdir -p "${HOST_REPOS_DIR}"
+    HOST_REPOS_DIR_DOCKER_PARAM="--volume ${HOST_REPOS_DIR}:${CONTAINER_HOME_DIR}"
+fi
+
+if [[ "${DEBUG}" = "true" ]]; then
+  echo "* Running container ${DOCKER_IMAGE_TAG} in DEBUG mode"
+  docker run                                                       \
+         --rm                                                      \
+         --interactive --tty --entrypoint /bin/bash                \
+         --user ${USER_IN_CONTAINER}                               \
+         --env SCRIPTS_LIB_DIR=${CONTAINER_HOME_DIR}/scripts/lib   \
+         --env OUTPUT_DIR="${CONTAINER_OUTPUT_DIR}"                \
+         --env RUN_TESTS="${RUN_TESTS}"                            \
+         --env GRAALVM_SUITE_RUNTIMES="${GRAALVM_SUITE_RUNTIMES}"  \
+         --env JAVA_OPTS="${DOCKER_JAVA_OPTS}"              \
+         --volume $(pwd):${CONTAINER_SCRIPTS_DIR}                  \
+         --volume $(pwd)/patch:${CONTAINER_HOME_DIR}/patch         \
+         --volume ${HOST_OUTPUT_DIR}:${CONTAINER_OUTPUT_DIR}       \
+         --memory ${DOCKER_MEMORY}                                              \
+         ${HOST_REPOS_DIR_DOCKER_PARAM}                            \
+         ${DOCKER_IMAGE_TAG}
+else
+  docker run                                                         \
+         --rm                                                        \
+         --user ${USER_IN_CONTAINER}                                 \
+         --entrypoint "${CONTAINER_HOME_DIR}/scripts/local-build.sh" \
+         --env SCRIPTS_LIB_DIR=${CONTAINER_HOME_DIR}/scripts/lib     \
+         --env OUTPUT_DIR="${CONTAINER_OUTPUT_DIR}"                  \
+         --env RUN_TESTS="${RUN_TESTS}"                              \
+         --env GRAALVM_SUITE_RUNTIMES="${GRAALVM_SUITE_RUNTIMES}"    \
+         --env JAVA_OPTS="${DOCKER_JAVA_OPTS}"                \
+         --volume $(pwd):${CONTAINER_SCRIPTS_DIR}                    \
+         --volume $(pwd)/patch:${CONTAINER_HOME_DIR}/patch           \
+         --volume ${HOST_OUTPUT_DIR}:${CONTAINER_OUTPUT_DIR}         \
+         --memory ${DOCKER_MEMORY}                                   \
+         ${HOST_REPOS_DIR_DOCKER_PARAM}                              \
+         ${DOCKER_IMAGE_TAG} &> ${BUILD_LOGS}
+fi
