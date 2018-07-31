4566b3c74a7c0bdeb8c22d8b815a034cf6bfa616
neomatrix369 <sadhak001@gmail.com>
2018-07-31 01:51:44 +0100

    Fixing the git clong process of graal-jvmci-8 project, also enable gitClone to also perform deep clones

--- build/x86_64/linux_macos/lib/build_JDK_JVMCI.sh
+++ build/x86_64/linux_macos/lib/build_JDK_JVMCI.sh
@@ -13,7 +13,7 @@ cd ${BASEDIR}
 gitClone graalvm       \
          graal-jvmci-8 \
          "Getting Graal JVMCI for JDK8" \
-         "doNotUpdate"
+         "deep"
 
 echo ">>> Building a JDK8 with JVMCI..."
 cd ${BASEDIR}/graal-jvmci-8/
