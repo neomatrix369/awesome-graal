6f9604a330202b998f2bcd188d308265059b30d4
mani <sadhak001@gmail.com>
2019-06-07 23:41:52 +0100

    Adding the BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG to the buildGraalCompiler.sh script to help toggle between verbose and non-verbose modes

--- build/x86_64/linux_macos/lib/buildGraalCompiler.sh
+++ build/x86_64/linux_macos/lib/buildGraalCompiler.sh
@@ -20,7 +20,9 @@ export JVMCI_VERSION_CHECK='ignore'
 echo ">>>> Setting environment variable JVMCI_VERSION_CHECK=${JVMCI_VERSION_CHECK}"
 HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS:-$(getAllowedThreads)}
 echo "Setting HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS}"
-HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS} ${MX} build
+echo "Setting BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG=${BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG:-}"
+
+HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS} ${MX} ${BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG:-} build 
 
 echo "Applying and checking patch to mx_jvmci.py..."
 git apply ${SCRIPTS_LIB_DIR}/patch/mx_compiler.py-VM-string-fix.patch || true
