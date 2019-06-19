7ea82138442d71c5e14ce4a720d5fe69155191bf
mani <sadhak001@gmail.com>
2019-06-19 01:34:11 +0100

    Debug: Add strace to enable debugging and capturing trace logs when graal compiler is built and archiving with the logs

--- build/x86_64/linux_macos/lib/buildGraalCompiler.sh
+++ build/x86_64/linux_macos/lib/buildGraalCompiler.sh
@@ -25,9 +25,13 @@ echo ">>>> Setting BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG=${BUILD_GRAAL_COMPILER
 export JAVA_OPTS="${MAX_HEAP_SIZE_FLAG:-} -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:ErrorFile=${BASEDIR}/hs_err_pid%p.log -XX:HeapDumpPath=${BASEDIR}/java-heap-dump-%p ${JAVA_OPTS:-}"
 echo ">>>> Setting JAVA_OPTS=${JAVA_OPTS}"
 
-set -x 
+set -x
 ${MX} --max-cpus ${MAX_CPUS} ${BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG:-} "-A-J${JAVA_OPTS}" build
 set +x
+
+echo "Applying and checking patch to mx_jvmci.py..."
+git apply ${SCRIPTS_LIB_DIR}/patch/mx_compiler.py-VM-string-fix.patch || true
+grep "pattern \= re.compile" -B 2 compiler/mx.compiler/mx_compiler.py || true
 
 MAX_CPUS=${MAX_CPUS:-$(getAvailableThreads)}
 echo ">>>> Setting MAX_CPUS=${MAX_CPUS}"
