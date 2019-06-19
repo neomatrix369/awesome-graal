e765ddf05d7004764b827b97fb464fd9db91af8d
mani <sadhak001@gmail.com>
2019-06-19 16:56:33 +0100

    Added patch to be applied to vm/mx.vm/mx_vm.py before the GraalVM suite is being built

--- build/x86_64/linux_macos/lib/buildGraalVMSuite.sh
+++ build/x86_64/linux_macos/lib/buildGraalVMSuite.sh
@@ -13,7 +13,21 @@ GRAALVM_SUITE_RUNTIMES=$3
 echo ">>> Building GraalVM Suite"
 echo "JAVA_OPTS=${JAVA_OPTS}"
 
+echo ">>>> Currently JAVA_HOME=${JAVA_HOME}"
+JDK8_JVMCI_HOME="$(cd ${BASEDIR}/graal-jvmci-8/ && ${MX} --java-home ${JAVA_HOME} jdkhome)"
+export JVMCI_VERSION_CHECK='ignore'
+export JAVA_HOME=${JDK8_JVMCI_HOME}
+echo ">>>> Newly set JAVA_HOME=${JAVA_HOME}"
+
 ${SCRIPTS_LIB_DIR}/applyPatches.sh ${BASEDIR}
 
 cd ${BASEDIR}/graal/vm
-${MX} --dy ${GRAALVM_SUITE_RUNTIMES} --J @"${JAVA_OPTS}" build
+
+echo "Applying and checking patch to mx/mx_vm.py..."
+git apply ${SCRIPTS_LIB_DIR}/patch/mx_vm.py-VM-version-string-fix.patch || true
+grep "pattern \= re.compile" -B 2 mx.vm/mx_vm.py                        || true
+
+set -x
+${MX} --dy ${GRAALVM_SUITE_RUNTIMES} \
+      --J @"${JAVA_OPTS}" --java-home=${JDK8_JVMCI_HOME} build
+set +x
