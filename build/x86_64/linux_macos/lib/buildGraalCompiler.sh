8cbaaac68862f5797a67a8d13e149a88c99ad9a1
mani <sadhak001@gmail.com>
2019-06-19 14:00:35 +0100

    Removed references to HOTSPOT_BUILD_JOBS from all scripts and config

--- build/x86_64/linux_macos/lib/buildGraalCompiler.sh
+++ build/x86_64/linux_macos/lib/buildGraalCompiler.sh
@@ -25,8 +25,6 @@ echo ">>>> Newly set JAVA_HOME=${JAVA_HOME}"
 cd ${BASEDIR}/graal/compiler
 export JVMCI_VERSION_CHECK='ignore'
 echo ">>>> Setting environment variable JVMCI_VERSION_CHECK=${JVMCI_VERSION_CHECK}"
-HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS:-$(getAllowedThreads)}
-echo ">>>> Setting HOTSPOT_BUILD_JOBS=${HOTSPOT_BUILD_JOBS}"
 echo ">>>> Setting BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG=${BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG:-}"
 export JAVA_OPTS="${MAX_HEAP_SIZE_FLAG:-} -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:ErrorFile=${BASEDIR}/hs_err_pid%p.log -XX:HeapDumpPath=${BASEDIR}/java-heap-dump-%p ${JAVA_OPTS:-}"
 echo ">>>> Setting JAVA_OPTS=${JAVA_OPTS}"
