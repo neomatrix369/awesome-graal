8f1fbbf2126cba1932085e5be4dc0bb54d40322c
mani <sadhak001@gmail.com>
2019-06-28 00:52:42 +0100

    Fixing unbound MAXCPUS in buildGraalCompiler.sh

--- build/x86_64/linux_macos/lib/buildGraalCompiler.sh
+++ build/x86_64/linux_macos/lib/buildGraalCompiler.sh
@@ -29,6 +29,9 @@ echo ">>>> Setting BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG=${BUILD_GRAAL_COMPILER
 export JAVA_OPTS="${MAX_HEAP_SIZE_FLAG:-} -XX:+HeapDumpOnOutOfMemoryError -XX:+ShowMessageBoxOnError -XX:ErrorFile=${BASEDIR}/hs_err_pid%p.log -XX:HeapDumpPath=${BASEDIR}/java-heap-dump-%p ${JAVA_OPTS:-}"
 echo ">>>> Setting JAVA_OPTS=${JAVA_OPTS}"
 
+MAX_CPUS=${MAX_CPUS:-$(getAvailableThreads)}
+echo ">>>> Setting MAX_CPUS=${MAX_CPUS}"
+
 set -x
 ${MX} --max-cpus ${MAX_CPUS} --java-home=${JDK8_JVMCI_HOME} \
       ${BUILD_GRAAL_COMPILER_VERBOSE_MODE_FLAG:-} "-A-J${JAVA_OPTS}" build
@@ -38,8 +41,6 @@ echo "Applying and checking patch to mx_jvmci.py..."
 git apply ${SCRIPTS_LIB_DIR}/patch/mx_compiler.py-VM-string-fix.patch || true
 grep "pattern \= re.compile" -B 2 compiler/mx.compiler/mx_compiler.py || true
 
-MAX_CPUS=${MAX_CPUS:-$(getAvailableThreads)}
-echo ">>>> Setting MAX_CPUS=${MAX_CPUS}"
 set -x
 ${MX} --max-cpus ${MAX_CPUS}          \
       --java-home=${JDK8_JVMCI_HOME}  \
