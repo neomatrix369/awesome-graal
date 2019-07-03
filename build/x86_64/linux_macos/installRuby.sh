a28398e085abcd208226884b4841cacaf284719c
mani <sadhak001@gmail.com>
2019-07-04 00:18:32 +0100

    Fixed the installRuby script to pick the right RVM_PATH depending on the target environment

--- build/x86_64/linux_macos/installRuby.sh
+++ build/x86_64/linux_macos/installRuby.sh
@@ -11,6 +11,12 @@ if [[ -f "/etc/sudoers" ]]; then
    RVM_SUDO_CMD=rvmsudo
 fi
 
+if [[ -f "/.dockerenv" ]]; then
+    export RVM_PATH=/usr/local/rvm
+else 
+    export RVM_PATH="/opt/$(whoami)/.rvm"
+fi
+
 RUBY_VERSION=${1:-"2.2.2"}
 
 ${SUDO_CMD} apt-get update && ${SUDO_CMD} apt-get install -qy \
@@ -45,7 +51,6 @@ curl -sSL https://rvm.io/mpapis.asc | gpg --no-tty --import -
 curl -sSL https://rvm.io/pkuczynski.asc | gpg --no-tty --import -
 curl -sSL https://get.rvm.io | bash -s stable
 
-export RVM_PATH="/usr/local/rvm"
 export PATH="${RVM_PATH}/bin:${PATH}"
 
 if [[ ! -z "${RVM_SUDO_CMD}" ]]; then
@@ -55,11 +60,14 @@ fi
 
 # Install RUBY
 echo "Installing ruby ${RUBY_VERSION} using rvm"
-${RVM_SUDO_CMD} rvm install ${RUBY_VERSION}
+set -x
+${RVM_SUDO_CMD} rvm install ${RUBY_VERSION} || (true && ${RVM_SUDO_CMD} rvm get stable)
 ${RVM_SUDO_CMD} rvm --default use ${RUBY_VERSION}
+set +x
 
-export RUBY_PATH="/usr/local/rvm/rubies/ruby-${RUBY_VERSION}"
-export GEM_HOME="/usr/local/rvm/gems/ruby-${RUBY_VERSION}"
-export GEM_PATH="/usr/local/rvm/gems/ruby-${RUBY_VERSION}"
+export RUBY_PATH="${RVM_PATH}/rubies/ruby-${RUBY_VERSION}"
+export GEM_HOME="${RVM_PATH}/gems/ruby-${RUBY_VERSION}"
+export GEM_PATH="${RVM_PATH}/gems/ruby-${RUBY_VERSION}"
 export PATH="${RUBY_PATH}/bin:${GEM_HOME}/bin:${PATH}"
+
 rvm info
