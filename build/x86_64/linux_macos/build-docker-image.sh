a3cd709562c42fcaa8b9d6b356762433c0fabb29
mani <sadhak001@gmail.com>
2019-07-04 03:05:18 +0100

    Passing the User name as build-args when build base and derived images

--- build/x86_64/linux_macos/build-docker-image.sh
+++ build/x86_64/linux_macos/build-docker-image.sh
@@ -18,7 +18,7 @@ echo "* Fetching docker image ${DOCKER_USER_NAME}/${JDK_PYTHON_BASE_IMAGE_TAG} f
 docker pull ${DOCKER_USER_NAME}/${JDK_PYTHON_BASE_IMAGE_TAG} || true
 docker build \
             -t ${DOCKER_USER_NAME}/${JDK_PYTHON_BASE_IMAGE_TAG}                      \
-            --build-arg JDK_BASE_IMAGE_TAG=${JDK_BASE_IMAGE_TAG} \
+            --build-arg JDK_BASE_IMAGE_TAG=${DOCKER_USER_NAME}/${JDK_BASE_IMAGE_TAG} \
             -f ${CURRENT_DIR}/Dockerfile-python-${PYTHON_VERSION:-2.7} .
 
 echo ""
@@ -32,7 +32,7 @@ docker build \
             -t ${DOCKER_USER_NAME}/${DOCKER_IMAGE_TAG} \
             --build-arg USER_IN_CONTAINER=${USER_IN_CONTAINER}                 \
             --build-arg GRAAL_JVMCI_8_TAG=${GRAAL_JVMCI_8_TAG}                 \
-            --build-arg JDK_PYTHON_BASE_IMAGE_TAG=${JDK_PYTHON_BASE_IMAGE_TAG} \
+            --build-arg JDK_PYTHON_BASE_IMAGE_TAG=${DOCKER_USER_NAME}/${JDK_PYTHON_BASE_IMAGE_TAG} \
             --build-arg MAKE_VERSION=${MAKE_VERSION}                           \
             --build-arg PYTHON_VERSION=${PYTHON_VERSION}                       \
             --build-arg LLVM_VERSION=${LLVM_VERSION}                           \
